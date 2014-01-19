12187f6bfeeba360ca2372e1c1b6919e6b68ae29
BostjanSkok <BostjanSkok2@gmail.com>
2014-01-19 14:08:21 +0100

    NoMultiThreadedTimeLoging

--- VpsaSem1/main.cpp
+++ VpsaSem1/main.cpp
@@ -1,23 +1,27 @@
 /*
- *  main.c
- *  RayTracer
- *
- *  Created by Patricio Bulic on 5/30/13.
- *  Copyright 2013 University of Ljubljana, Faculty of Computer and Information Science. All rights reserved.
- *
- */
+*  main.c
+*  RayTracer
+*
+*  Created by Patricio Bulic on 5/30/13.
+*  Copyright 2013 University of Ljubljana, Faculty of Computer and Information Science. All rights reserved.
+*
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <iostream>
+#include <windows.h>
 #include "defs.h"
 #include "vectors.h"
 #include "sphere.h"
 #include "shading.h"
 #include "ray.h"
+#include <iomanip>
 
 #include <GL/gl.h>
-#include <GL/glut.h>
+#include "glut.h"
+
+#include <omp.h>
 
 #define NSPHERES 4
 #define VIEWPLANE 400
@@ -54,8 +58,7 @@ double current_reflected_lambda = 0x7fefffffffffffff; // maximum positive double
 
 // window identifier:
 static int win;
-
-
+using namespace std;
 void Timer (int obsolete) {
 
 	glutPostRedisplay();
@@ -63,48 +66,48 @@ void Timer (int obsolete) {
 }
 
 void init(void) {
-	
+
 	direction[0] = up;
 	direction[1] = down;
 	direction[2] = up;
-	
+
 	/*pixel.i = 0;
 	pixel.j = 0;*/
-	
+
 	// set scene:
 	viewport.xvmin = -VIEWPLANE;
 	viewport.yvmin = -VIEWPLANE;
 	viewport.xvmax = VIEWPLANE;
 	viewport.yvmax = VIEWPLANE;
-	
+
 	camera_frame.u.x = 1.0;
 	camera_frame.u.y = 0.0;
 	camera_frame.u.z = 0.0;
-	
+
 	camera_frame.v.x = 0.0;
 	camera_frame.v.y = 1.0;
 	camera_frame.v.z = 0.0;
-	
+
 	camera_frame.n.x = 0.0;
 	camera_frame.n.y = 0.0;
 	camera_frame.n.z = 1.0;
-	
+
 	view_point.x = (viewport.xvmax - viewport.xvmin) / 2.0 ;
 	view_point.y = (viewport.yvmax - viewport.yvmin) / 2.0 ;
 	view_point.z = 0.0;
-	
-	
+
+
 	light.x = view_point.x - 1300;
 	light.y = view_point.y + 1300 ;
 	light.z = view_point.z - 300;
-	
-	
+
+
 	ambi_light_intensity = 1.0;
 	light_intensity = 1.0;
-	
+
 	focal_distance = FOCALDIST;
-	
-	
+
+
 	sphere[0].radius = RADIUS/1.5;
 	sphere[0].center.x  = view_point.x - (RADIUS+30);
 	sphere[0].center.y  = view_point.y ;
@@ -115,7 +118,7 @@ void init(void) {
 	set_rgb_array(sphere[0].ka_rgb, 0.0, 0.0, 0.2);
 	sphere[0].shininess = 100.0;
 	sphere[0].mirror = false;
-	
+
 	sphere[1].radius = RADIUS/1.2;
 	sphere[1].center.x  = view_point.x + 0;
 	sphere[1].center.y  = view_point.y + 50;
@@ -126,8 +129,8 @@ void init(void) {
 	set_rgb_array(sphere[1].ka_rgb, 0.0, 0.2, 0.0);
 	sphere[1].shininess = 10.0;
 	sphere[1].mirror = false;
-	
-	
+
+
 	sphere[2].radius = RADIUS;
 	sphere[2].center.x  = view_point.x + (2*RADIUS+30);
 	sphere[2].center.y  = view_point.y + 100;
@@ -138,8 +141,8 @@ void init(void) {
 	set_rgb_array(sphere[2].ka_rgb, 0.2, 0.0, 0.0);
 	sphere[2].shininess = 100.0;
 	sphere[2].mirror = false;
-	
-	
+
+
 	sphere[3].radius = 100*RADIUS;
 	sphere[3].center.x  = view_point.x ;
 	sphere[3].center.y  = view_point.y - 100*RADIUS-130;
@@ -150,8 +153,8 @@ void init(void) {
 	set_rgb_array(sphere[3].ka_rgb, 0.5, 0.5, 0.5);
 	sphere[3].shininess = 100.0;
 	sphere[3].mirror = true;
-	
-   // set clearing (background) color to white:
+
+	// set clearing (background) color to white:
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	// specify that ortgogonal 2D projection is to be used to 
 	// map context of 2D world coordinats to the screen. We use the 
@@ -162,8 +165,8 @@ void init(void) {
 }
 
 void animate(void){
-	
-	
+
+
 	// bounce the first ball:
 	if (direction[0] == up) {
 		if (sphere[0].center.y < viewport.yvmax+300) {
@@ -181,7 +184,7 @@ void animate(void){
 			direction[0] = up;
 		}
 	}
-	
+
 	// bounce the second ball:
 	if (direction[1] == up) {
 		if (sphere[1].center.y < viewport.yvmax+300) {
@@ -199,7 +202,7 @@ void animate(void){
 			direction[1] = up;
 		}
 	}
-	
+
 	// bounce the third ball:
 	if (direction[2] == up) {
 		if (sphere[2].center.y < viewport.yvmax+300) {
@@ -217,28 +220,32 @@ void animate(void){
 			direction[2] = up;
 		}
 	}
-	 
-	
-	
+
+
+
 	//light.z -= 100;
-	
+
 }
 
-
+int FrameCount = 0;
 void disp(void){
-	
+
 	//int i,j,k,l,m;
-	
+	FrameCount++;
 	animate();
-	
+
 	//clear all pixels:
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+	//lastTime = omp_get_wtime();
+	//cout <<"l" <<  omp_get_wtime() <<" \n"; //<< setprecision(4)c
+	//cout  << FrameCount << ";" << "Code;" << (lastTime - omp_get_wtime()) <<" \n"; //<< setprecision(4)c
+	//lastTime = omp_get_wtime();
+	double dispTime =  omp_get_wtime();
+
 	// RAY TRACING:
-	for (int i=0; i<(viewport.xvmax - viewport.xvmin); i++)
-	{
-		for (int j=0; j<(viewport.yvmax - viewport.yvmin); j++)
-		{
+	for (int i=0; i<(viewport.xvmax - viewport.xvmin); i++){
+
+		for (int j=0; j<(viewport.yvmax - viewport.yvmin); j++){
 			int intersection_object = -1; // none
 			int reflected_intersection_object = -1; // none
 			double current_lambda = 0x7fefffffffffffff; // maximum positive double
@@ -255,10 +262,11 @@ void disp(void){
 
 			pixel.i = i;
 			pixel.j = j;
-			
+
 			// 1. compute ray:
 			compute_ray(&ray, &view_point, &viewport, &pixel, &camera_frame, focal_distance);
-			
+
+			double lastTime = omp_get_wtime();
 			// 2. check if ray hits an object:
 			for (int k=0; k<NSPHERES; k++)
 			{
@@ -267,24 +275,32 @@ void disp(void){
 					// there is an intersection between ray and object
 					// 1. Izracunanaj normalu...
 					intersection_normal(&sphere[k], &intersection, &ray);
-				    // 2. ako je lambda presjecista manji od trenutacnog:
+					// 2. ako je lambda presjecista manji od trenutacnog:
 					if (intersection.lambda_in<current_lambda)
 					{
 						current_lambda=intersection.lambda_in;
 						intersection_object=k;
 						copy_intersection_struct(&current_intersection, &intersection);
 					}
-				     // izracunaj current lambda current_lambda =
-				     // oznaci koji je trenutacni object : intersection_object = 
-				     // kopiraj strukturu presjeka : copy_intersection_struct();
+					// izracunaj current lambda current_lambda =
+					// oznaci koji je trenutacni object : intersection_object = 
+					// kopiraj strukturu presjeka : copy_intersection_struct();
 				}
 			}
-			
+			cout  << FrameCount << ";" << "RayObjectColision;" << (lastTime - omp_get_wtime()) <<" \n"; //<< setprecision(4)c
+
+
+			cout  << FrameCount << ";" << "DispTimeBeforeColorAndOpenGl;" << (dispTime - omp_get_wtime()) <<" \n"; //<< setprecision(4)c
+
 			// Compute the color of the pixel:
 			if (intersection_object > -1)
 			{
 				compute_shadow_ray(&shadow_ray, &intersection, &light);
 				theta = dotproduct(&(shadow_ray.direction), &(intersection.normal));
+
+				lastTime = omp_get_wtime();
+
+
 				for (int l=0; l<NSPHERES; l++)
 				{
 					if (l!=intersection_object)
@@ -293,12 +309,16 @@ void disp(void){
 							bShadow=true;
 					}
 				}
+
+				cout  << FrameCount << ";" << "PixelColor;" << (lastTime - omp_get_wtime()) <<" \n"; //<< setprecision(4)c
+
+
 				if (bShadow)
 				{	// if in shadow, add only ambiental light to the surface color
 					red = shadow(sphere[intersection_object].ka_rgb[CRED], ambi_light_intensity);
 					green = shadow(sphere[intersection_object].ka_rgb[CGREEN], ambi_light_intensity);
 					blue = shadow(sphere[intersection_object].ka_rgb[CBLUE], ambi_light_intensity);
-				}
+				} 
 				else
 				{
 					// the intersection is not in shadow:
@@ -333,15 +353,15 @@ void disp(void){
 			current_reflected_lambda = 0x7fefffffffffffff;
 		}
 	}
+	cout  << FrameCount << ";" << "DispTime;" << (dispTime - omp_get_wtime()) <<" \n"; //<< setprecision(4)c
 	//glFlush();
 	glutSwapBuffers();
 }
 
 
 int main (int argc, char** argv) {
-	
-	
-	// init glut:
+	freopen( "C:\\Users\\Bostjan\\Documents\\file.csv", "w", stdout );
+	cout << "Frame;CodeSection;Time\n";
 	glutInit (&argc, argv);
 	// specify the display mode to be RGB and single buffering:
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -359,7 +379,7 @@ int main (int argc, char** argv) {
 	Timer(0);
 	// enter tha main loop and process events:
 	glutMainLoop();
-	
+
 	return 0;
 }
 
